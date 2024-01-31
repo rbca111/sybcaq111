2369e2dfc119e36682d97ceb5274831007db44bf
rbca111 <rbca22111.rcca@gmail.com>
2024-01-31 09:54:22 +0530

    hii arsh

---
+++ caseTools/src/new_repository111/caterpillar_111.java
@@ -0,0 +1,12 @@
+package new_repository111;
+
+public class caterpillar_111 {
+	int moves,cabbages;
+	
+	caterpillar_111(int moves, int cabbages)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+
+}
